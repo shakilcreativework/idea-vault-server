3d0688f14f76b11cb5935a63c290b9500bb9a3cc
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-05-21 08:53:40 +0600

    add user data show by email

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const dns = require("node:dns");
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
-const express = require('express');
+const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -14,36 +14,69 @@ const port = process.env.PORT || 5000;
 const uri = process.env.MONGODB_URI;
 
 // middleware
-app.use(cors()); 
-app.use(express.json()); 
+app.use(cors());
+app.use(express.json());
 
-// ----------------------- mongodb start --------------------------- 
+// ----------------------- mongodb start ---------------------------
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 async function run() {
   try {
     await client.connect();
     // ----------- server db code start --------------
-    const db = client.db('ideavaultDB');
-    const ideaCollection = db.collection('ideas');
+    const db = client.db("ideavaultDB");
+    const ideaCollection = db.collection("ideas");
 
     // get
-    app.get('/add-ideas', async(req, res) => {
+    app.get("/add-ideas", async (req, res) => {
       const result = await ideaCollection.find().toArray();
       console.log(result);
       res.json(result);
     });
 
+    app.get("/add-ideas/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const query = { _id: new ObjectId(id) };
+
+      const result = await ideaCollection.findOne(query);
+
+      res.send(result);
+    });
+
+    // Get ideas by user email
+    app.get("/add-ideas", async(req, res) => {
+      const email = req.query.email;
+      let query = {};
+
+      // If email exists, filter by email
+      if (email) {
+        query = {
+          userEmail: email,
+        };
+      }
+
+      const result = await ideaCollection.find(query).toArray();
+
+      res.json(result);
+    });
+
     // post
-    app.post('/add-ideas', async(req, res) => {
+    app.post("/add-ideas", async (req, res) => {
       const ideaData = req.body;
-      console.log(ideaData);
-      const result = await ideaCollection.insertOne(ideaData);
+
+      // Add current date & time
+      const newData = {
+        ...ideaData,
+        createdAt: new Date(),
+      };
+      console.log(newData);
+      const result = await ideaCollection.insertOne(newData);
       res.json(result);
     });
 
@@ -52,18 +85,20 @@ async function run() {
     // delete
     // ----------- server db code ends ---------------
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
   } finally {
     // await client.close();
   }
 }
 run().catch(console.dir);
-// ----------------------- mongodb ends ---------------------------- 
+// ----------------------- mongodb ends ----------------------------
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-})
+});
